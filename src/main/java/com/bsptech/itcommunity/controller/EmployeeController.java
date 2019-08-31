169692065b4b7f479b93785871c5cf75a1be64a1
Sarkhan Rasullu <bsptechou@gmail.com>
2019-08-31 19:39:54 +0400

    backend integrated frontend

--- src/main/java/com/bsptech/itcommunity/controller/EmployeeController.java
+++ src/main/java/com/bsptech/itcommunity/controller/EmployeeController.java
@@ -7,21 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("employees")
+@RequestMapping
 public class EmployeeController {
+
     @GetMapping
     public ModelAndView index(ModelAndView modelAndView) {
         modelAndView.setViewName("/employee/index");
         return modelAndView;
     }
 
-    @RequestMapping(path = "/{employeeId}")
+    @RequestMapping(path = "/employees/{employeeId}")
     public ModelAndView detail(@PathVariable("employeeId") Integer employeeId, ModelAndView modelAndView) {
         modelAndView.setViewName("/employee/details");
         return modelAndView;
     }
 
-    @RequestMapping(path = "/register")
+    @RequestMapping(path = "/employees/register")
     public ModelAndView register(ModelAndView modelAndView) {
         modelAndView.setViewName("/employee/registration");
         return modelAndView;
