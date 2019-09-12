e784437b848227d5c893fe329adf9b3ab49ffe24
goshgarmirzayev <qosqarmirze@gmail.com>
2019-09-12 13:17:53 +0400

    Search by Language maybe done

--- src/main/java/com/bsptech/itcommunity/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/bsptech/itcommunity/service/impl/EmployeeProfileServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bsptech.itcommunity.service.impl;
 
 import com.bsptech.itcommunity.dao.EmployeeProfileDataInter;
+import com.bsptech.itcommunity.dao.EmployeeProfileLanguageDataInter;
 import com.bsptech.itcommunity.entity.EmployeeProfile;
+import com.bsptech.itcommunity.entity.EmployeeProfileLanguage;
 import com.bsptech.itcommunity.entity.User;
 import com.bsptech.itcommunity.service.inter.EmployeeProfileServiceInter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import java.util.List;
 public class EmployeeProfileServiceImpl implements EmployeeProfileServiceInter {
     @Autowired
     EmployeeProfileDataInter employeeProfileDataInter;
+    @Autowired
+    EmployeeProfileLanguageDataInter employeeProfileLanguageDataInter;
 
     @Override
     public EmployeeProfile findById(Integer id) {
@@ -27,14 +31,27 @@ public class EmployeeProfileServiceImpl implements EmployeeProfileServiceInter {
 
     @Override
     public List<EmployeeProfile> findAll(EmployeeProfile e) {
-        
+
         List<EmployeeProfile> employeeProfileList = new ArrayList<>();
 
-        if(e.getUserId()==null){
-            employeeProfileList = (List<EmployeeProfile>) employeeProfileDataInter.findAll();            
-        } else {   
-            User u = e.getUserId();
-            employeeProfileList = employeeProfileDataInter.find(u.getName(),u.getSurname(),u.getEmail(),u.getPhone());
+        if (e.getUserId() == null) {
+            employeeProfileList = (List<EmployeeProfile>) employeeProfileDataInter.findAll();
+        }
+//        else {
+//            User u = e.getUserId();
+//            employeeProfileList = employeeProfileDataInter.find(u.getName(), u.getSurname(), u.getEmail(), u.getPhone());
+//        }
+        List<EmployeeProfileLanguage> fromView = new ArrayList<>();
+        List<EmployeeProfileLanguage> fromDatabase = new ArrayList<>();
+        if (e.getEmployeeProfileLanguageList() != null) {
+            fromView = e.getEmployeeProfileLanguageList();
+            for (EmployeeProfileLanguage empLang : fromView) {
+                fromDatabase = employeeProfileLanguageDataInter.findAllByLanguageIdAndLevel(empLang.getLanguageId(), empLang.getLevel());
+            }
+            for (EmployeeProfileLanguage empLangForAdd : fromDatabase) {
+                employeeProfileList.add(empLangForAdd.getEmployeeProfileId());
+            }
+
         }
 
         return employeeProfileList;
