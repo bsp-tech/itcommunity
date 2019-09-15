fc3852fcaeeb506f7a64bff92d848cd11aacfb83
Sarkhan Rasullu <bsptechou@gmail.com>
2019-09-15 15:21:01 +0400

    changes

--- src/main/java/com/bsptech/itcommunity/config/SecurityConfig.java
+++ src/main/java/com/bsptech/itcommunity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.bsptech.itcommunity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
