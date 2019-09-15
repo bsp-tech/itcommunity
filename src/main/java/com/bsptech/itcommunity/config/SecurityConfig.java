8916b7c74d950993e443a6d63435307b41a9756c
Sarkhan Rasullu <bsptechou@gmail.com>
2019-09-15 15:23:08 +0400

    changes

--- src/main/java/com/bsptech/itcommunity/config/SecurityConfig.java
+++ src/main/java/com/bsptech/itcommunity/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.bsptech.itcommunity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -26,15 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     @Qualifier("pwdEncoder")
     private PasswordEncoder passwordEncoder;
-
-//    public static void main(String[] args) {
-//        System.out.println(new BCryptPasswordEncoder().encode("111111"));
-//    }
-
-    @Bean("pwdEncoder")
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
